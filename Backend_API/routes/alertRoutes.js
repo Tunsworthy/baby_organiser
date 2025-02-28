89cc6fa1e31ee276b2068c753ab3eee26e305ace
Tom <mail@tomunsworth.net>
2025-02-28 20:30:11 +1100

    added in rate limiter

--- Backend_API/routes/alertRoutes.js
+++ Backend_API/routes/alertRoutes.js
@@ -10,10 +10,12 @@ const limiter = rateLimit({
     max: 100 // limit each IP to 100 requests per windowMs
 });
 
-router.get('/alerts', limiter, alertController.getAllAlerts); // Get all alerts
-router.get('/alerts/active', limiter, alertController.getActiveAlerts); // Get all active alerts
-router.get('/alerts/:id', limiter, alertController.getAlertById); // Get an alert by ID
-router.post('/alerts/', limiter, alertController.createAlert); // Create a new alert
-router.patch('/alerts/:id', limiter, alertController.updateAlertById); // Update an alert by ID
+router.use(limiter);
+
+router.get('/alerts', alertController.getAllAlerts); // Get all alerts
+router.get('/alerts/active', alertController.getActiveAlerts); // Get all active alerts
+router.get('/alerts/:id', alertController.getAlertById); // Get an alert by ID
+router.post('/alerts/', alertController.createAlert); // Create a new alert
+router.patch('/alerts/:id', alertController.updateAlertById); // Update an alert by ID
 
 module.exports = router;
